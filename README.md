cf1e55d7d0283797e52199e6ae5de6c00c796b74
Lucas Saliés Brum <sistematico@gmail.com>
2025-06-11 10:39:37 -0400

    Update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Curupira
+
+<div style="text-align: center;">
+    <img src="./assets/curupira.jpg" alt="Curupira" />
+</div>
---
+++ colors.sh
@@ -0,0 +1,49 @@
+# Cores básicas
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+CYAN="\033[36m"
+WHITE="\033[37m"
+
+# Cores brilhantes
+GRAY="\033[90m"
+BRED="\033[91m"
+BGREEN="\033[92m"
+BYELLOW="\033[93m"
+BBLUE="\033[94m"
+BMAGENTA="\033[95m"
+BCYAN="\033[96m"
+BWHITE="\033[97m"
+
+# Fundos básicos
+BGBLACK="\033[40mFundo Preto\033[0m"
+BGRED="\033[41mFundo Vermelho\033[0m"
+BGGREEN="\033[42mFundo Verde\033[0m"
+BGYELLOW="\033[43mFundo Amarelo\033[0m"
+BGBLUE="\033[44mFundo Azul\033[0m"
+BGMAGENTA="\033[45mFundo Magenta\033[0m"
+BGCYAN="\033[46mFundo Ciano\033[0m"
+BGWHITE="\033[47mFundo Branco\033[0m"
+
+# Fundos brilhantes
+BBGGRAY="\033[100mFundo Cinza Escuro\033[0m"
+BBGRED="\033[101mFundo Vermelho Claro\033[0m"
+BBGGREEN="\033[102mFundo Verde Claro\033[0m"
+BBGYELLOW="\033[103mFundo Amarelo Claro\033[0m"
+BBGBLUE="\033[104mFundo Azul Claro\033[0m"
+BBGMAGENTA="\033[105mFundo Magenta Claro\033[0m"
+BBGCYAN="\033[106mFundo Ciano Claro\033[0m"
+BBGWHITE="\033[107mFundo Branco Brilhante\033[0m"
+
+# Estilos de texto
+BOLD="\033[1mTexto Negrito\033[0m"
+FADED="\033[2mTexto Esmaecido\033[0m"
+ITALIC="\033[3mTexto Itálico\033[0m"
+UNDERLINE="\033[4mTexto Sublinhado\033[0m"
+INVERT="\033[7mTexto Invertido\033[0m"
+CROSSED="\033[9mTexto Riscado\033[0m"
+
+RESET="\033[0m"  # Reseta todas as cores e estilos
